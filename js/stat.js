dbb1acc796879701c08ad5f5027c04f9feb52758
Александр <sh889@ya.ru>
2020-09-27 13:06:56 +0500

    Учебный проект: холст

--- js/stat.js
+++ js/stat.js
@@ -8,13 +8,13 @@ const CLOUD_Y = 10; // начальная координата Y облака
 const SHADOW_GAP = 10; // отступ тени облака
 const GAP_DATA = 20; // отступ данных от края облака
 const FONT = `16px PT Mono`; // шрифт и размер текста
-const FONT_COLOR = `#000`; // цвет текста
+const FONT_COLOR = `rgba(0, 0, 0, 1)`; // цвет текста
 const FONT_GAP = 20; // межстрочный интервал
 const COLUMN_HEIGHT = 150; // высота колонки
 const COLUMN_WIDTH = 40; // ширина колонки
 const COLUMN_GAP = 50; // расстояние между колонками
 const COLUMN_COLOR_YOU = `rgba(255, 0, 0, 1)`; // цвет колонки игрока
-const COLUMN_COLOR_PLAYERS = 240; // цвет колонки других игроков
+const COLUMN_COLOR_PLAYERS = 240; // цвет колонки других игроков (цветовая модель HSL)
 
 function renderCloud(ctx, x, y, color) {
   ctx.fillStyle = color;
@@ -33,31 +33,12 @@ function renderCloud(ctx, x, y, color) {
   ctx.fill();
 }
 
-function getMaxElement(arr) {
-  let maxElement = arr[0];
-  for (let i = 1; i < arr.length; i++) {
-    if (arr[i] > maxElement) {
-      maxElement = arr[i];
-    }
-  }
-  return maxElement;
-}
-
 function getColumnColor(color, minSaturation, maxSaturation) {
   return `hsl(` + color + `, ` + Math.floor(Math.random() * (maxSaturation - minSaturation) + minSaturation) + `%, 50%)`;
 }
 
-window.renderStatistics = function (ctx, names, times) {
-  renderCloud(ctx, CLOUD_X + SHADOW_GAP, CLOUD_Y + SHADOW_GAP, `rgba(0, 0, 0, 0.7)`);
-  renderCloud(ctx, CLOUD_X, CLOUD_Y, `#fff`);
-
-  const maxTime = getMaxElement(times);
-  ctx.font = FONT;
-
-  ctx.fillStyle = FONT_COLOR;
-  ctx.textBaseline = `hanging`;
-  ctx.fillText(`Ура вы победили!`, CLOUD_X + GAP_DATA, CLOUD_Y + GAP_DATA);
-  ctx.fillText(`Cписок результатов:`, CLOUD_X + GAP_DATA, CLOUD_Y + GAP_DATA + FONT_GAP);
+function renderDiagram(ctx, names, times) {
+  const maxTime = times.reduce((previousValue, currentValue) => Math.max(previousValue, currentValue), times[0]);
 
   for (let i = 0; i < names.length; i++) {
     ctx.fillStyle = FONT_COLOR;
@@ -69,4 +50,18 @@ window.renderStatistics = function (ctx, names, times) {
     ctx.fillStyle = (names[i] === `Вы`) ? COLUMN_COLOR_YOU : getColumnColor(COLUMN_COLOR_PLAYERS, 30, 100);
     ctx.fillRect(CLOUD_X + COLUMN_GAP + (COLUMN_WIDTH + COLUMN_GAP) * i, CLOUD_Y + CLOUD_HEIGHT - GAP_DATA - FONT_GAP, COLUMN_WIDTH, -(COLUMN_HEIGHT * times[i] / maxTime));
   }
+}
+
+window.renderStatistics = function (ctx, names, times) {
+  renderCloud(ctx, CLOUD_X + SHADOW_GAP, CLOUD_Y + SHADOW_GAP, `rgba(0, 0, 0, 0.7)`);
+  renderCloud(ctx, CLOUD_X, CLOUD_Y, `rgba(255, 255, 255, 1)`);
+
+  ctx.font = FONT;
+  ctx.fillStyle = FONT_COLOR;
+  ctx.textBaseline = `hanging`;
+
+  ctx.fillText(`Ура вы победили!`, CLOUD_X + GAP_DATA, CLOUD_Y + GAP_DATA);
+  ctx.fillText(`Cписок результатов:`, CLOUD_X + GAP_DATA, CLOUD_Y + GAP_DATA + FONT_GAP);
+
+  renderDiagram(ctx, names, times);
 };
